6d821c1e27e6b3e65ab6466129d8b5031350ada1
Rusiru Erandaka <erandak-ec20037@stu.kln.ac.lk>
2025-06-17 17:13:58 +0530

    link the html form to the backend

--- main.py
+++ main.py
@@ -9,4 +9,31 @@ model = load_model('models/model-197.model')
 scaler_data = joblib.load('models/scaler_data.sav')
 scaler_target = joblib.load('models/scaler_target.sav')
 
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('details.html')
+@app.route('/getresults', methods=['POST'])
+def getresults():
 
+    results = request.form
+    print(results)
+    name = results['name']
+    gender = float(results['gender'])
+    age = float(results['age'])
+    tc = float(results['tc'])
+    hdl = float(results['hdl'])
+    smoke = float(results['smoke'])
+    bpm = float(results['bpm'])
+    diab = float(results['diab'])
+
+    test_data = np.array([gender, age, tc, hdl, smoke, bpm, diab]).reshape(1, -1)
+    test_data = scaler_data.transform(test_data)
+    prediction = model.predict(test_data)
+    prediction = scaler_target.inverse_transform(prediction)
+    print(prediction, prediction[0], prediction[0][0])
+
+    resultDict = {"name": name, "risk": round(prediction[0][0],2)}
+    return render_template('result.html', result=resultDict)
+
+app.run(debug=True)
