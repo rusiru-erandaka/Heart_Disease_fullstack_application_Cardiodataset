b0201dc11bc20a8ae2259ab1d77cac68bc037012
Rusiru Erandaka <erandak-ec20037@stu.kln.ac.lk>
2025-06-18 05:47:32 +0530

    do some changes to use .keras model

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import numpy as np
 from keras.models import load_model
 from keras import backend as K
 
-model = load_model('models/model-197.model')
+
+model = load_model('models/model-191.keras')
 
 scaler_data = joblib.load('models/scaler_data.sav')
 scaler_target = joblib.load('models/scaler_target.sav')
